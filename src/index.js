9a4a1bdd8069841ccec79eac3f7820b24229c8f1
nahyun <knahyuk@gmail.com>
2024-12-03 00:37:11 +0900

    remote repo name changed(homepage, router basename changed)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='React-netflix-clone-from-lecture'>
+  <BrowserRouter basename='Netflix-clone-with-react-lecture'>
     <App />
   </BrowserRouter>
 );
